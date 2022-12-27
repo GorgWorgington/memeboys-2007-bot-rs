bca81fcebe0b9d6983b47d213fa8cee54197b719
gorgworgington <gorgworg@gmail.com>
2022-12-26 20:15:10 -0800

    initial commit

---
+++ src/config.rs
@@ -0,0 +1,26 @@
+use serde::Deserialize;
+
+#[derive(Debug)]
+pub enum ConfigError {
+  ParseFailure,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Config {
+  discord_token: String,
+}
+
+impl Config {
+  pub fn from_json(config_json: String) -> Result<Config, ConfigError> {
+    let res: Result<Config, _> = serde_json::from_str(&*config_json);
+
+    match res {
+      Ok(cfg) => Ok(cfg),
+      Err(_) => Err(ConfigError::ParseFailure),
+    }
+  }
+
+  pub fn token(&self) -> String {
+    self.discord_token.to_owned()
+  }
+}
